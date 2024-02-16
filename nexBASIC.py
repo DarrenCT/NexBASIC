9b208a43639aa0cce0988f59aeb49e332dbf625d
DarrenCT <99516347+DarrenCT@users.noreply.github.com>
2024-02-16 17:47:39 -0500

    completed basic parser class
    the parser class can now create a parse tree with the correct order of operations for + - * /.

--- nexBASIC.py
+++ nexBASIC.py
@@ -160,44 +160,55 @@ class BinOpNode:
 
 # Parser
 
+# Parser
 class Parser:
-	def __init__(self, tokens):
-		self.tokens = tokens
-		self.tok_idx = -1
-		self.advance()
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.tok_idx = -1
+        self.advance()
 
-def advance(self):
-		self.tok_idx += 1
-		if self.tok_idx < len(self.tokens):
-			self.current_tok = self.tokens[self.tok_idx]
-		return self.current_tok
+    def advance(self):
+        self.tok_idx += 1
+        if self.tok_idx < len(self.tokens):
+            self.current_tok = self.tokens[self.tok_idx]
+        return self.current_tok
 
+    def parse(self):
+        res = self.expr()
+        return res
 
-def factor(self):
-    tok = self.current_tok
-    # check to see if number is an int or float
-    if tok.type in (TT_INT, TT_FLOAT):
-         self.advance()
-         return NumberNode(tok)
+    def factor(self):
+        tok = self.current_tok
+        # check to see of number is int or float
+        if tok.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(tok)
 
+    def term(self):
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
 
-def term(self):
-    return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+    def expr(self):
+        return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
 
-def expr(self):
-    return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
+    def bin_op(self, func, ops):
+        left = func()
+        while self.current_tok.type in ops:
+            op_tok = self.current_tok
+            self.advance()
+            right = func()
+            left = BinOpNode(left, op_tok, right)
+        return left
 
-def bin_op(self, func, ops):
-    left = func()
-
-    while self.current_tok in ops:
-         op_tok = self.current_tok
-         right = func()
-         left = BinOpNode(left, op_tok, right)
-    return left
 
 # Run Function
 def run(fn, text):
+    # Generate Tokens
     lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
-    return tokens, error
+    if error: return None, error
+
+    # Generate sytax tree
+    parser = Parser(tokens)
+    ast = parser.parse()
+
+    return ast, None
